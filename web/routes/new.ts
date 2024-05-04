e1921478756330febcfe775922cbc2db0be7febe
Shea Leslein <sleslein@users.noreply.github.com>
2024-05-04 09:25:52 -0500

    wip add feature

--- web/routes/new.ts
+++ web/routes/new.ts
@@ -2,6 +2,8 @@ import { Hono } from "https://deno.land/x/hono@v4.0.9/mod.ts";
 import { Layout } from "../views/Layout.ts";
 import { SiteData } from "../views/Layout.ts";
 import { html } from "https://deno.land/x/hono@v4.0.9/helper.ts";
+import { ArcherySession } from "../../app/ArcherySession.ts";
+import { DecodedArrow } from "../../app/arrow-value.ts";
 
 const app = new Hono();
 
@@ -11,31 +13,114 @@ const siteData: SiteData = {
 };
 
 app.get("/", async (context) => {
-
-  return context.html(Layout({ ...siteData,
+  return context.html(Layout({
+    ...siteData,
     children: html`
       <h1>New Session</h1>
       <form method="post" action="new" class="table rows">
-        <p><label for="date">Date</label><input name="date" type="date" /></p>
-        <p>
-          <label for="distance">Distance</label>
-          <select name='distance'>
-            <option value="">--</option>
-            <option value="20">20 yards</option>
-            <option value="30">30 yards</option>
-          </select>
-        </p>
-        <p>
-          <label for='arrows'>Arrows</label>
-          <textarea name="arrows"></textarea>
-        </p>
-        <p><button type="submit">Save</button></p>
+      <p><label for="date">Date</label><input name="date" type="date" /></p>
+      <p>
+        <label for="distance">Distance</label>
+        <select name='distance'>
+          <option value="">--</option>
+          <option value="20">20 yards</option>
+          <option value="30">30 yards</option>
+        </select>
+      </p>
+      <p> 
+        <label for="arrowInput">Encoded Arrow</label>
+        <input 
+          id='arrowInput' 
+          name="arrowInput" 
+          type="text" 
+        />
+        <button type="button" onclick='handleArrowAdd()'>Add</button>
+      </p>
+      <input type="text" 
+        id="arrows" 
+        hx-get="/new/calc"
+        hx-trigger="change"
+        hx-target="#details"
+      />
+      <p><button type="submit">Save</button></p>
       </form>
+      <section>
+
+        <h2>Details</h2>
+        <div id="details">
+          <ul id='arrowList'></ul>
+          <div>
+            <span>Total Arrows: </span>
+            <span id="totalArrows">
+          </div>
+          <div>
+            <span>Average: </span>
+            <span id="statsAvg">
+          </div>
+        </div>
+      </section>
+      ${EditDialog()}
+      <script src='/static/js/client-new.js'>
+      </script>
     `
   }));
 });
 
+app.post("/calc", async (context) => {
+  console.log(`calc/called`);
+  const body = await context.req.parseBody();
+  console.log(JSON.stringify(body['arrows']));
+  const inputArrows = body['arrows'].toString();
+
+  const session = new ArcherySession(`9999-12-12 0 ${inputArrows}`)
+  const arrows = session.arrows;
+
+  const arrowListEl = ArrowList(arrows);
+  return context.html(`
+  <div>
+    ${arrowListEl}
+  </div>`);
+});
+
+function ArrowList(arrows: Array<[boolean, DecodedArrow]>) {
+  return `<ul>
+    ${arrows.map((decodedArrow, idx) => {
+    return ArrowListItem(decodedArrow, idx);
+  }).join('')
+    }
+  </ul>`
+}
+
+function ArrowListItem(decodedArrow: [boolean, DecodedArrow], idx: number) {
+  const [isvalid, arrowVal] = decodedArrow;
+  return `
+    <li${!isvalid ? ' class="bad color bg"' : ''}>
+      ${arrowVal.encodedValue}
+      <span role="button" class="<a> text-sm" data-arrow-index="${idx}" onClick="openEditDialog(${idx})">Edit</span>
+    </li>
+  `;
+}
+
+function EditDialog() {
+  return html`<dialog id='edit-dialog' >
+      <input id='edit-arrow-index' type="hidden" />
+      <input id="edit-arrow" type="input" />
+      <button onClick="editArrow()">Update</button>
+    </dialog>`;
+}
+
 app.post('/', async (context) => {
+  const body = await context.req.parseBody();
+  const requestSession = {
+    date: body['date'].toString(),
+    distance: body['distance'].toString(),
+    arrows: [...body['arrows'].toString().split(' ')]
+  };
+
+  const encodedSession = ArcherySession.encodeSession(requestSession);
+  const session = new ArcherySession(encodedSession);
+  session.save();
+
   return context.redirect('/list', 303);
 })
 
--- web/views/Layout.ts
+++ web/views/Layout.ts
@@ -20,6 +20,7 @@ export function Layout({ title, description, children }: SiteData) {
             <meta property="og:title" content="${title}">
             <link rel="stylesheet" href="https://unpkg.com/missing.css@1.1.1">
             <link rel='stylesheet' type="text/css" href="/static/global.css" />
+            <script src="/static/js/vendor/htmx-1.9.12.min.js"></script>
         </head>
         <body>
           <main>
